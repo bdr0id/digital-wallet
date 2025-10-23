5997910cefeb4d7a195afb14f08733647dc2bc8a
bdr0id <collinskboit@gmail.com>
2025-10-23 12:23:53 +0400

    add main application class and startup configuration

--- src/main/java/com/boit_droid/wallet/WalletApplication.java
+++ src/main/java/com/boit_droid/wallet/WalletApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WalletApplication {
+public class 	WalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApplication.class, args);
